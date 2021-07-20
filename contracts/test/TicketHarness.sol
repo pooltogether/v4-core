6f75e92d4a6177e4deb4ff566d7e57ed411df0f5
Pierrick Turelier <pierrick@turelier.com>
2021-07-20 18:31:02 -0500

    feat(Ticket): add getBalance function

---
+++ contracts/test/TicketHarness.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.6;
+
+import '../Ticket.sol';
+
+contract TicketHarness is Ticket {
+  function getBalances(address user) external view returns (Balance[32] memory) {
+    return balances[user];
+  }
+
+  function mint(address to, uint256 amount) external {
+    _mint(to, amount);
+  }
+}
