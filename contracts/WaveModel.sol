d8f7f0573ef2a11d686694cbb1bfb435d5046766
Aodhgan <aodhgan@pooltogether.com>
2021-07-21 16:42:01 -0700

    fixed test

---
+++ contracts/WaveModel.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+import "hardhat/console.sol";
+import "./TsunamiPrizeStrategy.sol";
+// Libraries & Inheritance
+
+
+
+// contract WaveModel is IWaveModel {
+  
+
+// }
