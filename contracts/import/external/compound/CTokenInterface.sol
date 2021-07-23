d26fb11cf53c5df21371bd56f64d633f8d4ea896
kamescg <kamescg@gmail.com>
2021-07-23 13:11:15 -0700

    import required v3.4.0 contracts

---
+++ contracts/import/external/compound/CTokenInterface.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+interface CTokenInterface is IERC20Upgradeable {
+    function decimals() external view returns (uint8);
+    function totalSupply() external override view returns (uint256);
+    function underlying() external view returns (address);
+    function balanceOfUnderlying(address owner) external returns (uint256);
+    function supplyRatePerBlock() external returns (uint256);
+    function exchangeRateCurrent() external returns (uint256);
+    function mint(uint256 mintAmount) external returns (uint256);
+    function redeem(uint256 amount) external returns (uint256);
+    function balanceOf(address user) external override view returns (uint256);
+    function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
+}
---
+++ contracts/import/prize-strategy/BeforeAwardListener.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.6;
+
+import "./BeforeAwardListenerInterface.sol";
+import "../Constants.sol";
+import "./BeforeAwardListenerLibrary.sol";
+
+abstract contract BeforeAwardListener is BeforeAwardListenerInterface {
+  function supportsInterface(bytes4 interfaceId) external override view returns (bool) {
+    return (
+      interfaceId == Constants.ERC165_INTERFACE_ID_ERC165 || 
+      interfaceId == BeforeAwardListenerLibrary.ERC165_INTERFACE_ID_BEFORE_AWARD_LISTENER
+    );
+  }
+}
