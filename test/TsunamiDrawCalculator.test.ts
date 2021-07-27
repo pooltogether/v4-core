4df4beda17835c1b8622237a81c3405bcaaa117d
Pierrick Turelier <pierrick@turelier.com>
2021-07-27 18:54:56 -0500

    chore(solhint): fix compiler version

--- test/TsunamiDrawCalculator.test.ts
+++ test/TsunamiDrawCalculator.test.ts
@@ -25,7 +25,7 @@ describe('TsunamiDrawCalculator', () => {
     let wallet2: any;
     let wallet3: any;
 
-    
+
     let draw: any
     const encoder = ethers.utils.defaultAbiCoder
 
@@ -38,13 +38,13 @@ describe('TsunamiDrawCalculator', () => {
         ticket = await deployMockContract(wallet1, ticketArtifact.abi)
 
         const matchCardinality = 3
-        
+
         await drawCalculator.initialize(ticket.address, matchCardinality, [ethers.utils.parseEther("0.2"), ethers.utils.parseEther("0.8")])
 
     })
 
     describe('calculate()', () => {
-      it.only('should calculate', async () => {
+      it('should calculate', async () => {
         //function calculate(address user, uint256[] calldata randomNumbers, uint256[] calldata timestamps, uint256[] calldata prizes, bytes calldata data) external override view returns (uint256){
 
         const winningNumber = utils.solidityKeccak256(["address"], [wallet1.address])//"0x1111111111111111111111111111111111111111111111111111111111111111"
@@ -52,7 +52,7 @@ describe('TsunamiDrawCalculator', () => {
         const userRandomNumber = utils.solidityKeccak256(["bytes32", "uint256"],[winningNumber, 1])
         console.log("userRandomNumber in test", userRandomNumber)
 
-        
+
         const pickIndices = encoder.encode(["uint256[][]"], [[["1"]]])
 
         await ticket.mock.getBalances.withArgs(wallet1.address, [42]).returns([10])
