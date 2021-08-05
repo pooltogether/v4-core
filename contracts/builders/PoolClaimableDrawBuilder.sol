24e2cba43c0c8a1a852298db633a2c561d80e1fb
kamescg <kamescg@gmail.com>
2021-08-05 11:39:46 -0700

    add compound prizepool

--- contracts/builders/PoolClaimableDrawBuilder.sol
+++ contracts/builders/PoolClaimableDrawBuilder.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.6.12;
+pragma solidity 0.8.6;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts-upgradeable/utils/SafeCastUpgradeable.sol";
@@ -72,7 +72,7 @@ contract PoolWithMultipleWinnersBuilder {
     multipleWinnersBuilder = _multipleWinnersBuilder;
   }
 
-  function createCompoundMultipleWinners(
+  function createCompoundClaimableDrawPrizeStrategy(
     CompoundPrizePoolConfig memory prizePoolConfig,
     MultipleWinnersBuilder.MultipleWinnersConfig memory prizeStrategyConfig,
     uint8 decimals
@@ -101,64 +101,7 @@ contract PoolWithMultipleWinnersBuilder {
     return prizePool;
   }
 
-  function createYieldSourceMultipleWinners(
-    YieldSourcePrizePoolConfig memory prizePoolConfig,
-    MultipleWinnersBuilder.MultipleWinnersConfig memory prizeStrategyConfig,
-    uint8 decimals
-  ) external returns (YieldSourcePrizePool) {
-    YieldSourcePrizePool prizePool = yieldSourcePrizePoolProxyFactory.create();
-    MultipleWinners prizeStrategy = multipleWinnersBuilder.createMultipleWinners(
-      prizePool,
-      prizeStrategyConfig,
-      decimals,
-      msg.sender
-    );
-    prizePool.initializeYieldSourcePrizePool(
-      reserveRegistry,
-      _tokens(prizeStrategy),
-      prizePoolConfig.maxExitFeeMantissa,
-      prizePoolConfig.yieldSource
-    );
-    prizePool.setPrizeStrategy(prizeStrategy);
-    prizePool.setCreditPlanOf(
-      address(prizeStrategy.ticket()),
-      prizeStrategyConfig.ticketCreditRateMantissa.toUint128(),
-      prizeStrategyConfig.ticketCreditLimitMantissa.toUint128()
-    );
-    prizePool.transferOwnership(msg.sender);
-    emit YieldSourcePrizePoolWithMultipleWinnersCreated(prizePool, prizeStrategy);
-    return prizePool;
-  }
-
-  function createStakeMultipleWinners(
-    StakePrizePoolConfig memory prizePoolConfig,
-    MultipleWinnersBuilder.MultipleWinnersConfig memory prizeStrategyConfig,
-    uint8 decimals
-  ) external returns (StakePrizePool) {
-    StakePrizePool prizePool = stakePrizePoolProxyFactory.create();
-    MultipleWinners prizeStrategy = multipleWinnersBuilder.createMultipleWinners(
-      prizePool,
-      prizeStrategyConfig,
-      decimals,
-      msg.sender
-    );
-    prizePool.initialize(
-      reserveRegistry,
-      _tokens(prizeStrategy),
-      prizePoolConfig.maxExitFeeMantissa,
-      prizePoolConfig.token
-    );
-    prizePool.setPrizeStrategy(prizeStrategy);
-    prizePool.setCreditPlanOf(
-      address(prizeStrategy.ticket()),
-      prizeStrategyConfig.ticketCreditRateMantissa.toUint128(),
-      prizeStrategyConfig.ticketCreditLimitMantissa.toUint128()
-    );
-    prizePool.transferOwnership(msg.sender);
-    emit StakePrizePoolWithMultipleWinnersCreated(prizePool, prizeStrategy);
-    return prizePool;
-  }
-
+ 
   function _tokens(MultipleWinners _multipleWinners) internal view returns (ControlledTokenInterface[] memory) {
     ControlledTokenInterface[] memory tokens = new ControlledTokenInterface[](2);
     tokens[0] = ControlledTokenInterface(address(_multipleWinners.ticket()));
