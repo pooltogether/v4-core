d5f73d5586b6713bb3be1749374a3476fc8cd018
kamescg <kamescg@gmail.com>
2021-08-20 13:43:43 -0700

    unit tests for user claim payout history

--- contracts/test/ClaimableDrawHarness.sol
+++ contracts/test/ClaimableDrawHarness.sol
@@ -11,11 +11,48 @@ contract ClaimableDrawHarness is ClaimableDraw {
     return _createDraw(randomNumber, timestamp, prize);
   } 
 
-  // function readLastClaimFromClaimedHistory(bytes32 _userClaimedDraws, uint8 _drawIndex) external pure returns (bool) {
-  //   return _readUsersDrawClaimStatusFromClaimedHistory(_userClaimedDraws, _drawIndex);
-  // }
+  function createNewDraws(uint256[] calldata randomNumbers, uint32[] calldata timestamps, uint256[] calldata prizes) external returns (uint256) {
+    for (uint256 index = 0; index < randomNumbers.length; index++) {
+      return _createDraw(randomNumbers[index], timestamps[index], prizes[index]);
+    }
+  } 
 
-  // function writeLastClaimFromClaimedHistory(bytes32 _userClaimedDraws, uint8 _drawIndex) external pure returns (bytes32) {
-  //   return _writeUsersDrawClaimStatusFromClaimedHistory(_userClaimedDraws, _drawIndex);
-  // }
+  function drawIdToClaimIndex(uint256 drawId, uint256 _currentDrawId) external pure returns (uint256) {
+    return _drawIdToClaimIndex(drawId, _currentDrawId);
+  } 
+
+  function calculateDrawCollectionPayout(
+    address _user,
+    uint96[CARDINALITY] memory _userClaimedDraws, 
+    uint256[] calldata _drawIds, 
+    IDrawCalculator _drawCalculator, 
+    bytes calldata _data
+  ) external returns (uint256 totalPayout, uint96[CARDINALITY] memory userClaimedDraws) {
+    return _calculateDrawCollectionPayout(_user, _userClaimedDraws, _drawIds, _drawCalculator, _data);
+  } 
+
+  function createDrawClaimsInput(
+    uint256[] calldata _drawIds, 
+    IDrawCalculator _drawCalculator,
+    uint256[] memory _randomNumbers,
+    uint32[] memory _timestamps,
+    uint256[] memory _prizes
+  ) external view returns (uint256[] memory, uint32[] memory, uint256[] memory) {
+    return _createDrawClaimsInput(_drawIds, _drawCalculator, _randomNumbers, _timestamps, _prizes);
+  } 
+  
+  function validateDrawPayout(
+    uint96[CARDINALITY] memory _userClaimedDraws, 
+    uint256 _drawId, 
+    uint96 _payout
+  ) external view returns (uint96, uint96[CARDINALITY] memory) {
+    return _validateDrawPayout(_userClaimedDraws, _drawId, _payout);
+  } 
+
+  function setUserDrawPayoutHistory(address user, uint96[CARDINALITY] memory userClaimedDraws, uint256 _nextDrawId) external returns (bool) {
+    nextDrawId = _nextDrawId;
+    userPayoutHistory[user] = userClaimedDraws;
+    return true;
+  } 
+
 }
