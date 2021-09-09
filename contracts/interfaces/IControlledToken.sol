fdf4f6d987e3323aa1da3ee5eb682ad245863e48
kamescg <kamescg@gmail.com>
2021-09-09 15:03:40 -0700

    cleanup interface variables

--- contracts/interfaces/IControlledToken.sol
+++ contracts/interfaces/IControlledToken.sol
@@ -21,20 +21,20 @@ interface IControlledToken is IERC20Upgradeable {
 
   /// @notice Allows the controller to mint tokens for a user account
   /// @dev May be overridden to provide more granular control over minting
-  /// @param _user Address of the receiver of the minted tokens
-  /// @param _amount Amount of tokens to mint
-  function controllerMint(address _user, uint256 _amount) external;
+  /// @param user Address of the receiver of the minted tokens
+  /// @param amount Amount of tokens to mint
+  function controllerMint(address user, uint256 amount) external;
 
   /// @notice Allows the controller to burn tokens from a user account
   /// @dev May be overridden to provide more granular control over burning
-  /// @param _user Address of the holder account to burn tokens from
-  /// @param _amount Amount of tokens to burn
-  function controllerBurn(address _user, uint256 _amount) external;
+  /// @param user Address of the holder account to burn tokens from
+  /// @param amount Amount of tokens to burn
+  function controllerBurn(address user, uint256 amount) external;
 
   /// @notice Allows an operator via the controller to burn tokens on behalf of a user account
   /// @dev May be overridden to provide more granular control over operator-burning
-  /// @param _operator Address of the operator performing the burn action via the controller contract
-  /// @param _user Address of the holder account to burn tokens from
-  /// @param _amount Amount of tokens to burn
-  function controllerBurnFrom(address _operator, address _user, uint256 _amount) external;
+  /// @param operator Address of the operator performing the burn action via the controller contract
+  /// @param user Address of the holder account to burn tokens from
+  /// @param amount Amount of tokens to burn
+  function controllerBurnFrom(address operator, address user, uint256 amount) external;
 }
--- contracts/interfaces/ITicket.sol
+++ contracts/interfaces/ITicket.sol
@@ -41,7 +41,7 @@ interface ITicket {
   
   function getBalanceAt(address user, uint256 timestamp) external view returns(uint256);
   function getBalancesAt(address user, uint32[] calldata timestamp) external view returns(uint256[] memory);
-  function getAverageBalanceBetween(address _user, uint256 _startTime, uint256 _endTime) external view returns (uint256);
+  function getAverageBalanceBetween(address user, uint256 startTime, uint256 endTime) external view returns (uint256);
   function getTotalSupply(uint32 timestamp) external view returns(uint256);
   function getTotalSupplies(uint32[] calldata timestamp) external view returns(uint256[] memory);
 }
