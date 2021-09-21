3c8ec1dd3480c2f376a3696eb8a1c7ff474ae96c
kamescg <kamescg@gmail.com>
2021-09-21 07:52:10 -0400

    DrawRingBufferLib: add library tests in exposed harness

---
+++ contracts/test/DrawRingBufferExposed.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.6;
+import "../libraries/DrawRingBuffer.sol";
+
+/**
+  * @title  Expose the DrawRingBufferLibrary for unit tests
+  * @author PoolTogether Inc.
+ */
+contract DrawRingBufferLibExposed {
+  using DrawRingBuffer for DrawRingBuffer.Buffer;
+
+
+  uint16 public constant MAX_CARDINALITY = 256;
+  DrawRingBuffer.Buffer internal drawRingBuffer;
+
+  constructor(uint8 _cardinality) {
+    drawRingBuffer.cardinality = _cardinality;
+  }
+
+  function _push(DrawRingBuffer.Buffer memory _buffer, uint32 _drawId) external view returns (DrawRingBuffer.Buffer memory) {
+    return DrawRingBuffer.push(_buffer, _drawId);
+  }
+
+  function _getIndex(DrawRingBuffer.Buffer memory _buffer, uint32 _drawId) external view returns (uint32) {
+    return DrawRingBuffer.getIndex(_buffer, _drawId);
+  }
+
+}
