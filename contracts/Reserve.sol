1188ca65c09c9e17e97aea3c073c971b1f24112d
kamescg <kamescg@gmail.com>
2021-09-24 10:47:53 -0400

    Reserve: add notes and natspec

--- contracts/Reserve.sol
+++ contracts/Reserve.sol
@@ -10,6 +10,11 @@ import "@pooltogether/owner-manager-contracts/contracts/Manageable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+/**
+  * @title  PoolTogether V4 Reserve
+  * @author PoolTogether Inc Team
+  * @notice The Reserve migrates allotments of token distributions.
+*/
 contract Reserve is IReserve, Manageable {
     using SafeERC20 for IERC20;
     
@@ -33,23 +38,31 @@ contract Reserve is IReserve, Manageable {
     }
 
     /* ============ External Functions ============ */
-
-    function withdrawTo(address _recipient, uint256 _amount) external override onlyManagerOrOwner {
-        _checkpoint();
-
-        token.safeTransfer(_recipient, _amount);
-        withdrawAccumulator += uint224(_amount);
-        
-        emit Withdrawn(_recipient, _amount);
-    }
     
+    /**
+      * @notice Create observation checkpoint in ring bufferr.
+      * @dev    Calculates total desposited tokens since last checkpoint and creates new accumulator checkpoint.
+     */
     function checkpoint() external override {
         _checkpoint();
-    }    
+    }
+
+    /**
+      * @notice Read global CARDINALITY value.
+      * @return Ring buffer range (i.e. CARDINALITY) 
+     */
+    function getCardinality() external view returns (uint16) {
+        return cardinality;
+    }
     
+    /**
+      * @notice Calculate token accumulation beween timestamp range.
+      * @dev    Search the ring buffer for two checkpoint observations and diffs accumulator amount. 
+      * @param _startTimestamp Account address 
+      * @param _endTimestamp   Transfer amount
+     */
     function getReserveAccumulatedBetween(uint32 _startTimestamp, uint32 _endTimestamp) external override view returns (uint224) {
         require(_startTimestamp < _endTimestamp, "Reserve/start-less-then-end");
-        uint32 timeNow = uint32(block.timestamp);
         uint16 _cardinality = cardinality;
 
         ObservationLib.Observation memory _newestObservation;
@@ -64,6 +77,7 @@ contract Reserve is IReserve, Manageable {
             _cardinality,
             _startTimestamp
         );
+
         uint224 _end = _getReserveAccumulatedAt(
             _newestObservation,
             _oldestObservation,
@@ -71,14 +85,36 @@ contract Reserve is IReserve, Manageable {
             _endTimestamp
         );
 
-        // cases where we have observations between startTimestamp and endTimestamp
         return _end - _start;
     }
 
-    function getCardinality() external view returns (uint16) {
-        return cardinality;
+    /**
+      * @notice Transfer Reserve token balance to recipient address.
+      * @dev    Creates checkpoint before token transfer. Increments withdrawAccumulator with amount.
+      * @param _recipient Account address 
+      * @param _amount    Transfer amount
+     */
+    function withdrawTo(address _recipient, uint256 _amount) external override onlyManagerOrOwner {
+        _checkpoint();
+
+        token.safeTransfer(_recipient, _amount);
+        withdrawAccumulator += uint224(_amount);
+        
+        emit Withdrawn(_recipient, _amount);
     }
 
+    /* ============ Internal Functions ============ */
+
+    /**
+      * @notice Find optimal observation checkpoint using target timestamp
+      * @dev    Uses binary search if target timestamp is within ring buffer range.
+      * @param _newestObservation ObservationLib.Observation
+      * @param _oldestObservation ObservationLib.Observation
+      * @param _cardinality       RingBuffer Range
+      * @param timestamp          Timestamp target
+      *
+      * @return Optimal reserveAccumlator for timestamp.
+     */
     function _getReserveAccumulatedAt(
         ObservationLib.Observation memory _newestObservation,
         ObservationLib.Observation memory _oldestObservation,
@@ -86,55 +122,81 @@ contract Reserve is IReserve, Manageable {
         uint32 timestamp
     ) internal view returns (uint224) {
         uint32 timeNow = uint32(block.timestamp);
-        if (_cardinality == 0) {
-            return 0;
-        }
 
+        // IF empty ring buffer exit early.
+        if (_cardinality == 0) return 0;
+
+        /**
+          * Ring Buffer Search Optimization
+          * Before performing binary search on the ring buffer check 
+          * to see if timestamp is within range of [o T n] by comparing
+          * the target timestamp to the oldest/newest observation.timestamps
+          * IF the timestamp is out of the ring buffer range avoid starting
+          * a binary search, because we can return NULL or oldestObservation.amount
+        */
+
+        /**
+          * IF oldestObservation.timestamp is after timestamp: T[old ]
+          * the Reserve did NOT have a balance or the ring buffer
+          * no longer contains that timestamp checkpoint.
+         */
         if(_oldestObservation.timestamp > timestamp) {
             return 0;
         }
 
+        /**
+          * IF newestObservation.timestamp is before timestamp: [ new]T
+          * return _newestObservation.amount since observation is
+          * contains the highest checkpointed reserveAccumulator.
+         */
         if(_newestObservation.timestamp <= timestamp) {
             return _newestObservation.amount;
         }
         
+        // IF the timestamp is witin range of ring buffer start/end: [new T old]
+        // FIND the closest observation to the left(or exact) of timestamp: [OT ]
         (ObservationLib.Observation memory beforeOrAt, ObservationLib.Observation memory atOrAfter) = 
             ObservationLib.binarySearch(reserveAccumulators, _cardinality - 1, 0, timestamp, _cardinality, timeNow);
         
+        // IF target timestamp is EXACT match for atOrAfter.timestamp observation return amount.
+        // NOT having an exact match with atOrAfter means values will contain accumulator value AFTER the searchable range.
         if(atOrAfter.timestamp == timestamp) {
             return atOrAfter.amount;
         }
+
+        // ELSE return observation.totalDepositedAccumlator closest to LEFT of target timestamp.
         else {
             return beforeOrAt.amount;      
         }
     }
 
-    /* ============ Internal Functions ============ */
     function _checkpoint() internal {
-        uint256 balanceOfReserve = token.balanceOf(address(this));
-        uint224 _withdrawAccumulator = withdrawAccumulator; //sload
-
-        ObservationLib.Observation memory _reserveAccumulators;
-
         uint256 _cardinality = cardinality;
+        uint256 _balanceOfReserve = token.balanceOf(address(this));
+        uint224 _withdrawAccumulator = withdrawAccumulator; //sload
+        ObservationLib.Observation memory _newestObservation = _getNewestObservation(_cardinality);
 
-        if (_cardinality > 0) {
-            _reserveAccumulators = reserveAccumulators[_cardinality - 1]; 
-        }
-
-        if(balanceOfReserve + _withdrawAccumulator > _reserveAccumulators.amount) {
-            
+        /**
+          * IF tokens have been deposited into Reserve contract since the last checkpoint
+          * create a new Reserve balance checkpoint. The will will update multiple times in a single block.
+         */
+        if(_balanceOfReserve + _withdrawAccumulator > _newestObservation.amount) {
             uint32 now = uint32(block.timestamp);
-            uint224 newReserveAccumulator = uint224(balanceOfReserve) + _withdrawAccumulator;
             
-            if(_reserveAccumulators.timestamp != now) {
-                // store next observation
+            // checkpointAccumulator = currentBalance + totalWithdraws
+            uint224 newReserveAccumulator = uint224(_balanceOfReserve) + _withdrawAccumulator;
+            
+            // IF _newestObservation IS NOT in the current block.
+            // CREATE observation in the accumulators ring buffer.
+            if(_newestObservation.timestamp != now) {
                 reserveAccumulators[_cardinality] = ObservationLib.Observation({
                     amount: newReserveAccumulator, 
                     timestamp: now
                 });
                 cardinality++;
             }
+            // ELSE IF _newestObservation IS in the current block.
+            // UPDATE the checkpoint previously created in block history.
             else {
                 reserveAccumulators[_cardinality - 1] = ObservationLib.Observation({
                     amount: newReserveAccumulator, 
@@ -144,6 +206,10 @@ contract Reserve is IReserve, Manageable {
             emit Checkpoint(newReserveAccumulator, _withdrawAccumulator);
         }        
     }   
+
+    function _getNewestObservation(uint256 _cardinality) internal view returns (ObservationLib.Observation memory _observation) {
+        if (_cardinality > 0) _observation = reserveAccumulators[_cardinality - 1]; 
+    }
 
 
 }
--- test/Reserve.test.ts
+++ test/Reserve.test.ts
@@ -10,20 +10,17 @@ describe('Reserve', () => {
   let wallet3: SignerWithAddress;
   let reserve: Contract;
   let ticket: Contract;
+  let ReserveHarnessFactory: ContractFactory;
+  let erc20MintableFactory: ContractFactory;
 
   before(async () => {
     [wallet1, wallet2, wallet3] = await getSigners();
+    erc20MintableFactory = await ethers.getContractFactory('ERC20Mintable');
+    ReserveHarnessFactory = await ethers.getContractFactory('ReserveHarness');
   });
 
   beforeEach(async () => {
-    const ReserveHarnessFactory: ContractFactory = await ethers.getContractFactory(
-      'ReserveHarness',
-    );
-
-    const erc20MintableFactory: ContractFactory = await ethers.getContractFactory(
-      'ERC20Mintable',
-    );
-    ticket = await erc20MintableFactory.deploy('Dai Stablecoin', 'DAI');
+    ticket = await erc20MintableFactory.deploy('Ticket', 'TICK');
 
     reserve = await ReserveHarnessFactory.deploy(wallet1.address, ticket.address);
   });
@@ -44,9 +41,9 @@ describe('Reserve', () => {
 
     it('will succeed creating checkpoint with positive balance and after withdrawal', async () => {
       await ticket.mint(reserve.address, toWei('100'))
-      
+
       await reserve.withdrawTo(wallet2.address, toWei('100'))
-      
+
       await ticket.mint(reserve.address, toWei('100'))
       await expect(reserve.checkpoint())
         .to.emit(reserve, 'Checkpoint')
@@ -71,31 +68,27 @@ describe('Reserve', () => {
 
   })
 
-  describe('withdrawTo()', () => {
-
-  })
-
   describe('getReserveAccumulatedBetween()', () => {
     context('with one observation', () => {
-      it('start and end before observations', async() => {
+      it('start and end before observations', async () => {
         // s e |
         await reserve.setObservationsAt([{ timestamp: 5, amount: 70 }])
         expect(await reserve.getReserveAccumulatedBetween(2, 3)).to.equal(0)
       })
 
-      it('start before and end at observation', async() => {
+      it('start before and end at observation', async () => {
         // s e|
         await reserve.setObservationsAt([{ timestamp: 5, amount: 70 }])
         expect(await reserve.getReserveAccumulatedBetween(2, 5)).to.equal(70)
       })
 
-      it('start and end around observation', async() => {
+      it('start and end around observation', async () => {
         // s | e
         await reserve.setObservationsAt([{ timestamp: 5, amount: 70 }])
         expect(await reserve.getReserveAccumulatedBetween(2, 6)).to.equal(70)
       })
 
-      it('start at and end after observation', async() => {
+      it('start at and end after observation', async () => {
         // s| e
         await reserve.setObservationsAt([{ timestamp: 5, amount: 70 }])
         expect(await reserve.getReserveAccumulatedBetween(5, 6)).to.equal(0)
@@ -103,7 +96,7 @@ describe('Reserve', () => {
     })
 
     context('with two observations', () => {
-      it('start and end before observations', async() => {
+      it('start and end before observations', async () => {
         // s e [] 
         const observations = [
           {
@@ -119,8 +112,8 @@ describe('Reserve', () => {
         const result = await reserve.getReserveAccumulatedBetween(2, 3)
         expect(result).to.equal(0)
       })
-  
-      it('start before and end inside', async() => {
+
+      it('start before and end inside', async () => {
         // s [ e ]
         const observations = [
           {
@@ -136,84 +129,97 @@ describe('Reserve', () => {
         const result = await reserve.getReserveAccumulatedBetween(2, 6)
         expect(result).to.equal(70)
       })
-  
-      it('start before and end at first observation', async() => {
+
+      it('start before and end at first observation', async () => {
         // s [e ]
         const observations = [{
-            timestamp: 5,
-            amount: 70
-          },
-          {
-            timestamp: 8,
-            amount: 72
-          }]
+          timestamp: 5,
+          amount: 70
+        },
+        {
+          timestamp: 8,
+          amount: 72
+        }]
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(2, 5)
         expect(result).to.equal(70)
       })
 
-      it('start before and end at second observation', async() => {
+      it('start before and end at second observation', async () => {
         // s [ e]
-        const observations = [{timestamp: 5,amount: 70},{timestamp: 8,amount: 72}]
+        const observations = [{ timestamp: 5, amount: 70 }, { timestamp: 8, amount: 72 }]
 
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(2, 8)
         expect(result).to.equal(72)
       })
-    
-      it('both start and end inside', async() => {
+
+      it('both start and end inside', async () => {
         // [ s e ]
-        const observations = [{timestamp: 5,amount: 70},{timestamp: 8,amount: 72}]
+        const observations = [{ timestamp: 5, amount: 70 }, { timestamp: 8, amount: 72 }]
 
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(6, 7)
         expect(result).to.equal(0)
       })
 
-      it('start inside and end at second', async() => {
+      it('start inside and end at second', async () => {
         // [ s e]
-        const observations = [{timestamp: 5,amount: 70},{timestamp: 8,amount: 72}]
+        const observations = [{ timestamp: 5, amount: 70 }, { timestamp: 8, amount: 72 }]
 
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(6, 8)
         expect(result).to.equal(2)
       })
 
-      it('start at first and end at second', async() => {
+      it('start at first and end at second', async () => {
         // [s e]
-        const observations = [{timestamp: 5,amount: 70},{timestamp: 8,amount: 72}]
+        const observations = [{ timestamp: 5, amount: 70 }, { timestamp: 8, amount: 72 }]
 
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(5, 8)
-        expect(result).to.equal(2) 
+        expect(result).to.equal(2)
       })
 
-      it('start inside and end after', async() => {
+      it('start inside and end after', async () => {
         // [ s ] e
-        const observations = [{timestamp: 5,amount: 70},{timestamp: 8,amount: 72}]
+        const observations = [{ timestamp: 5, amount: 70 }, { timestamp: 8, amount: 72 }]
 
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(6, 10)
         expect(result).to.equal(2)
       })
 
-      it('start at end and end after', async() => {
+      it('start at end and end after', async () => {
         // [ s] e
-        const observations = [{timestamp: 5,amount: 70},{timestamp: 8,amount: 72}]
+        const observations = [{ timestamp: 5, amount: 70 }, { timestamp: 8, amount: 72 }]
 
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(8, 29)
         expect(result).to.equal(0)
       })
 
-      it('start after and end after', async() => {
+      it('start after and end after', async () => {
         // [] s e 
-        const observations = [{timestamp: 5,amount: 70},{timestamp: 8,amount: 72}]
+        const observations = [{ timestamp: 5, amount: 70 }, { timestamp: 8, amount: 72 }]
 
         await reserve.setObservationsAt(observations)
         const result = await reserve.getReserveAccumulatedBetween(18, 29)
         expect(result).to.equal(0)
-      })  
+      })
+    })
+  })
+
+  describe('withdrawTo()', () => {
+    it('should emit Checkpoint, Transfer and Withdrawn events', async () => {
+      await ticket.mint(reserve.address, toWei('100'))
+      expect(reserve.withdrawTo(wallet2.address, toWei('10')))
+        .to.emit(reserve, 'Checkpoint')
+        .withArgs(toWei('100'), 0)
+        .and.to.emit(ticket, 'Transfer')
+        .withArgs(wallet2.address, reserve.address, toWei('10'))
+        .and.to.emit(reserve, 'Withdrawn')
+        .withArgs(reserve.address, toWei('10'))
     })
   })
 })
