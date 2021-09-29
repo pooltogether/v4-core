7f5d35b48eece0384eafb06fb4b7a649b4b01391
Aodhgan <aodhgan@pooltogether.com>
2021-09-29 14:56:40 -0700

    changes

--- contracts/prize-pool/PrizePool.sol
+++ contracts/prize-pool/PrizePool.sol
@@ -105,19 +105,19 @@ abstract contract PrizePool is IPrizePool, Ownable, ReentrancyGuard, IERC721Rece
     uint256 ticketTotalSupply = _ticketTotalSupply();
     uint256 currentAwardBalance = _currentAwardBalance; //sload
 
-
     // it's possible for the balance to be slightly less due to rounding errors in the underlying yield source
     uint256 currentBalance = _balance();
     uint256 totalInterest = (currentBalance > ticketTotalSupply) ? currentBalance - ticketTotalSupply : 0;
     uint256 unaccountedPrizeBalance = (totalInterest > currentAwardBalance) ? totalInterest - currentAwardBalance : 0;
 
     if (unaccountedPrizeBalance > 0) {
-      _currentAwardBalance = currentAwardBalance + unaccountedPrizeBalance;
+      currentAwardBalance = currentAwardBalance + unaccountedPrizeBalance;
+      _currentAwardBalance = currentAwardBalance; //sstore
 
       emit AwardCaptured(unaccountedPrizeBalance);
     }
 
-    return _currentAwardBalance;
+    return currentAwardBalance;
   }
 
   /// @inheritdoc IPrizePool
