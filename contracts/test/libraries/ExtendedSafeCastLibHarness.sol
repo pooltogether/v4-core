ec1e547eb5027b6c5323be8f9dcb25d89ae9b59c
Pierrick Turelier <pierrick@turelier.com>
2021-09-30 18:28:35 -0500

    fix(contracts): rename ExtendedSafeCast

--- contracts/test/libraries/ExtendedSafeCastLibHarness.sol
+++ contracts/test/libraries/ExtendedSafeCastLibHarness.sol
@@ -2,10 +2,10 @@
 
 pragma solidity 0.8.6;
 
-import "../../libraries/ExtendedSafeCast.sol";
+import "../../libraries/ExtendedSafeCastLib.sol";
 
-contract ExtendedSafeCastHarness {
-    using ExtendedSafeCast for uint256;
+contract ExtendedSafeCastLibHarness {
+    using ExtendedSafeCastLib for uint256;
 
     function toUint208(uint256 value) external pure returns (uint208) {
         return value.toUint208();
