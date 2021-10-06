69bab9c42d8519b2f35c3358bdf3ec6bd249665a
Aodhgan <aodhgan@pooltogether.com>
2021-10-05 17:35:11 -0700

    dont import unused contracts

--- contracts/DrawBeacon.sol
+++ contracts/DrawBeacon.sol
@@ -3,10 +3,9 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
-// import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-// import "@openzeppelin/contracts/utils/Address.sol";
+
 import "@pooltogether/pooltogether-rng-contracts/contracts/RNGInterface.sol";
 import "@pooltogether/owner-manager-contracts/contracts/Ownable.sol";
 
